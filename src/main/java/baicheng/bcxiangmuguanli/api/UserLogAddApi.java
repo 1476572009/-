fb86463a6324c17a9d3a6b6d2b0557fcbe40cb98
yz <1476572009@qq.com>
2019-10-22 11:05:35 +0800

    第三次上传

--- src/main/java/baicheng/bcxiangmuguanli/api/UserLogAddApi.java
+++ src/main/java/baicheng/bcxiangmuguanli/api/UserLogAddApi.java
@@ -3,7 +3,6 @@ package baicheng.bcxiangmuguanli.api;
 
 import baicheng.bcxiangmuguanli.model.Userjron;
 import baicheng.bcxiangmuguanli.service.UserLogInterface;
-import baicheng.bcxiangmuguanli.vo.Auth;
 import baicheng.bcxiangmuguanli.vo.JsonResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
